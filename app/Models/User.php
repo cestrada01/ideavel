33342bdb8a5f9cbd805efd3b2ffb633be9ecb747
carestrada <cestrada@mendoza.gov.ar>
2024-09-20 12:00:44 -0300

    200924_1

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,9 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
@@ -44,4 +47,13 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function ideas():HasMany{
+        return $this->hasMany((Idea::class));
+    }
+
+    public function ideasLiked():BelongsToMany{
+        return $this->belongsToMany((Idea::class));
+    }
+
 }
