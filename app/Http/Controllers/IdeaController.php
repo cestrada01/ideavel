229fe8f68c2d274929fc5ae127e630e78225b6fd
carestrada <cestrada@mendoza.gov.ar>
2024-10-08 17:40:04 -0300

    081024

--- app/Http/Controllers/IdeaController.php
+++ app/Http/Controllers/IdeaController.php
@@ -1,12 +1,14 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\Idea;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class IdeaController extends Controller
 {
     public function index(){
-        return view('ideas.index');
+        $ideas = DB::table('ideas')->get();
+        return view('ideas.index',['ideas' => $ideas]);
     }
 }
